2469983ff2bd8c519d7b174fcc211f34f90931e7
Nahuel Montero <nahue.montero94@gmail.com>
2023-01-31 16:39:25 -0300

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import React,{ReactElement, useState} from 'react'
-import "./app.css"
+import React, { ReactElement, useState } from "react";
+import "./style.css";
 
-import Characters from './views/character'
-import Home from './views/home'
+import Characters from "./views/character";
+import Home from "./views/home";
 
-function App() : ReactElement{
-
-  const [loadImages, setLoadImages] = useState<boolean>(false)  
-
+function App(): ReactElement {
+  const [loadImages, setLoadImages] = useState<boolean>(false);
 
   //CAMBIAR NOMBRE A ESTA FUNCION
   const parentFunction = () => {
-      setLoadImages(!loadImages)
-  }
+    setLoadImages(!loadImages);
+  };
 
   return (
     <div className="App">
       <p className="title">Rick & Mroty</p>
-      {!loadImages ? 
-        <Home parentFunction = {parentFunction}/>:
-        <Characters parentFunction = {parentFunction}/>
-      }
+      {!loadImages ? (
+        <Home parentFunction={parentFunction} />
+      ) : (
+        <Characters parentFunction={parentFunction} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
