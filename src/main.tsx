dd417213f69a54492f1cd95a3df0cdf3d5ff243f
Nahuel Montero <nahue.montero94@gmail.com>
2023-01-31 16:30:28 -0300

    testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { ReactElement, useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
+import "./app.css";
+
+import Characters from "./views/character";
+import Home from "./views/home";
+
+function App(): ReactElement {
+  const [loadImages, setLoadImages] = useState<boolean>(false);
+
+  //CAMBIAR NOMBRE A ESTA FUNCION
+  const parentFunction = () => {
+    setLoadImages(!loadImages);
+  };
+
+  return (
+    <div className="App">
+      <p className="title">Rick & Mroty</p>
+      {!loadImages ? (
+        <Home parentFunction={parentFunction} />
+      ) : (
+        <Characters parentFunction={parentFunction} />
+      )}
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
